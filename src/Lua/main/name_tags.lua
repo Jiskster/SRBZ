36e46f9c4abeacf1a6f63237ef521b53cd619de4
Jisk <37682565+Jiskster@users.noreply.github.com>
2023-07-22 11:57:54 -0500

    Update name_tags.lua

--- src/Lua/main/name_tags.lua
+++ src/Lua/main/name_tags.lua
@@ -1,14 +1,50 @@
 --Original script by wired-aunt
 --Heavily edited version by Jisk.
+freeslot("S_POSS_PAIN","S_POSS_PAIN2","S_SPOS_PAIN","S_SPOS_PAIN2")
+
+--MF2_FRET
+states[S_POSS_PAIN] = {
+	sprite = SPR_POSS,
+	frame = A,
+	tics = 24,
+	nextstate = S_POSS_PAIN2
+}
+
+states[S_POSS_PAIN2] = {
+	sprite = SPR_POSS,
+	frame = A,
+	tics = 0,
+	action = A_SetObjectFlags2,
+	var1 = MF2_FRET,
+	var2 = 1,
+	nextstate = S_POSS_RUN1
+}
+states[S_SPOS_PAIN] = {
+	sprite = SPR_SPOS,
+	frame = A,
+	tics = 15,
+	nextstate = S_SPOS_PAIN2
+}
+
+states[S_SPOS_PAIN2] = {
+	sprite = SPR_POSS,
+	frame = A,
+	tics = 0,
+	var1 = MF2_FRET,
+	var2 = 1,
+	action = A_SetObjectFlags2,
+	nextstate = S_SPOS_RUN1
+}
 
 mobjinfo[MT_BLUECRAWLA].npc_name = "Blue Crawla"
 mobjinfo[MT_BLUECRAWLA].min_spawnhealth = 2
-mobjinfo[MT_BLUECRAWLA].max_spawnhealth = 7
+mobjinfo[MT_BLUECRAWLA].max_spawnhealth = 4
+mobjinfo[MT_BLUECRAWLA].painstate = S_POSS_PAIN 
 
 mobjinfo[MT_REDCRAWLA].npc_name = "Red Crawla"
 mobjinfo[MT_REDCRAWLA].min_spawnhealth = 5
 mobjinfo[MT_REDCRAWLA].max_spawnhealth = 12
-
+mobjinfo[MT_REDCRAWLA].painstate = S_SPOS_PAIN
 
 local sorted_mobjs = {}
 
